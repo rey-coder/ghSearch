ee45575fba6e5e3e1ca5a3aaffe3d2df81631ba7
Reinhard Otieno <reinhard@techlait.com>
2022-03-17 13:15:31 +0300

    Fixed query params update bug

--- src/app/pages/search/search-results/search-shell.component.ts
+++ src/app/pages/search/search-results/search-shell.component.ts
@@ -17,9 +17,12 @@ export class SearchShellComponent implements OnInit {
   searchSubject!: Subscription;
   totalCount: number = 1000;
 
-  constructor(private route: ActivatedRoute, private searchSvc: GHSearchService) {
+  constructor(private route: ActivatedRoute, private searchSvc: GHSearchService, private router: Router) {
     this.route.queryParams.subscribe((params: any) => {
-      if (params && 'q' in params) { this.queryString = params.q }
+      if (params && 'q' in params) { 
+        this.queryString = params.q;
+        this.DoSearch();
+      }
     });
   }
 
@@ -29,21 +32,25 @@ export class SearchShellComponent implements OnInit {
   DoSearch(evt?: any, opts?: IGHFilterOpts) {
     if (!opts && evt && evt.keyCode != 13) {
       return;
+    } else if (evt && evt.keyCode == 13) {
+      this.router.navigate(['/search'], {queryParams: {q: this.queryString}});
+    } else {
+      this.searchSvc.searchResults.next([]);
+      if (this.searchSubject !== undefined && !this.searchSubject.closed) {
+        this.searchSubject?.unsubscribe();
+      }
+  
+      this.searchSubject = this.searchSvc.UserSearch(this.queryString, opts)
+      .subscribe((res: any) => {
+        this.searchResults = res.items;
+        this.totalCount = res.total_count > this.totalCount ? this.totalCount: res.total_count;
+        this.searchSvc.searchResults.next(this.searchResults);
+        scrollTo(0,0);
+      }, (ex: any) => {
+        if (!this.searchResults.length) { this.searchResults= []; }
+      });
     }
-    this.searchSvc.searchResults.next([]);
-    if (this.searchSubject !== undefined && !this.searchSubject.closed) {
-      this.searchSubject?.unsubscribe();
-    }
-
-    this.searchSubject = this.searchSvc.UserSearch(this.queryString, opts)
-    .subscribe((res: any) => {
-      this.searchResults = res.items;
-      this.totalCount = res.total_count > this.totalCount ? this.totalCount: res.total_count;
-      this.searchSvc.searchResults.next(this.searchResults);
-      scrollTo(0,0);
-    }, (ex: any) => {
-      if (!this.searchResults.length) { this.searchResults= []; }
-    });
+    
   }
 
   ChangePage(evt: any) {
